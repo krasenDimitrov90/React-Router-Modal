9f307f2ea14ad27d752e4de9b0c6244221cdcdcc
krasenDimitrov <krasendimitrov90@gmail.com>
2024-08-27 14:15:33 +0300

    making the modal router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,210 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  useLocation,
+  useNavigate,
+  useParams,
+  NavLink,
+} from "react-router-dom";
 
-function App() {
+import { IMAGES, getImageById } from "./images";
+
+
+export default function App() {
+  let location = useLocation();
+
+  // The `backgroundLocation` state is the location that we were at when one of
+  // the gallery links was clicked. If it's there, use it as the location for
+  // the <Routes> so we show the gallery in the background, behind the modal.
+  let state = location.state || {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes location={state.backgroundLocation || location}>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="/img/:id" element={<ImageView />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+
+      {/* Show the modal when a `backgroundLocation` is set */}
+      {state.backgroundLocation && (
+        <Routes>
+          <Route path="/img/:id" element={<Modal />} />
+        </Routes>
+      )}
     </div>
   );
 }
 
-export default App;
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/gallery">Gallery</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <hr />
+
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+
+      <h3>Featured Images</h3>
+      <ul>
+        <li>
+          <Link to="/img/1">Image 1</Link>
+        </li>
+        <li>
+          <Link to="/img/2">Image 2</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Gallery() {
+  let location = useLocation();
+
+  return (
+    <div style={{ padding: "0 24px" }}>
+      <h2>Gallery</h2>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+          gap: "24px",
+        }}
+      >
+        {IMAGES.map((image) => (
+          <Link
+            key={image.id}
+            to={`/img/${image.id}`}
+            // This is the trick! Set the `backgroundLocation` in location state
+            // so that when we open the modal we still see the current page in
+            // the background.
+            state={{ backgroundLocation: location }}
+          >
+            <img
+              width={200}
+              height={200}
+              style={{
+                width: "100%",
+                aspectRatio: "1 / 1",
+                height: "auto",
+                borderRadius: "8px",
+              }}
+              src={image.src}
+              alt={image.title}
+            />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ImageView() {
+  let { id } = useParams();
+  let image = getImageById(Number(id));
+
+  if (!image) return <div>Image not found</div>;
+
+  return (
+    <div>
+      <h1>{image.title}</h1>
+      <img width={400} height={400} src={image.src} alt="" />
+    </div>
+  );
+}
+
+function Modal() {
+  let navigate = useNavigate();
+  let { id } = useParams();
+  let image = getImageById(Number(id));
+  let buttonRef = React.useRef(null);
+
+  function onDismiss() {
+    navigate(-1);
+  }
+
+  if (!image) return null;
+
+  return (
+    <Dialog
+      aria-labelledby="label"
+      onDismiss={onDismiss}
+      initialFocusRef={buttonRef}
+    >
+      <div
+        style={{
+          display: "grid",
+          justifyContent: "center",
+          padding: "8px 8px",
+        }}
+      >
+        <h1 id="label" style={{ margin: 0 }}>
+          {image.title}
+        </h1>
+        <img
+          style={{
+            margin: "16px 0",
+            borderRadius: "8px",
+            width: "100%",
+            height: "auto",
+          }}
+          width={400}
+          height={400}
+          src={image.src}
+          alt=""
+        />
+        <button
+          style={{ display: "block" }}
+          ref={buttonRef}
+          onClick={onDismiss}
+        >
+          Close
+        </button>
+      </div>
+    </Dialog>
+  );
+}
+
+const Dialog = ({ children }) => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="modal-test" onClick={() => navigate(-1)}>
+      {children}
+    </div>
+  );
+};
+
+function NoMatch() {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+}
